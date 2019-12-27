3491a05ea1833cb26ac4ab85d29615930023b91e
Paul Scarrone <paul.scarrone@gmail.com>
2019-12-26 19:17:19 -0500

    Adjusted versions for phalcon and psr based on php version deps
    The nuance is that formula can have an @ 'AT' version defined to allow for some sort of mechanism
    to have multiple versions of the same formula. Unfortunately AT is generally used for the version of the lib.
    
    In the case of psr i am just versioning against php version.
    
    I would like to eventually fork this tap by php version.
    That way each formula can be created a single time and the end user can specify
    which tap they want to employ assuring that updates always apply to the same php version

---
+++ Formula/phalcon@73_4.0.0.rb
@@ -0,0 +1,29 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class PhalconAT73400 < AbstractPhp73Extension
+  init
+  desc "Full-stack PHP framework"
+  homepage "https://phalconphp.com/"
+  url "https://github.com/phalcon/cphalcon/archive/v4.0.0.tar.gz"
+  sha256 "9d3fbaac4235110e000b8020543d53342aa7d274926dc44207753bc201121d7b"
+  head "https://github.com/phalcon/cphalcon.git"
+
+  bottle do
+    cellar :any_skip_relocation
+  end
+
+  depends_on "pcre"
+  depends_on "psr@73"
+
+  def install
+    Dir.chdir "build/php7/64bits"
+
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-phalcon"
+    system "make"
+
+    prefix.install "modules/phalcon.so"
+    write_config_file if build.with? "config-file"
+  end
+end
