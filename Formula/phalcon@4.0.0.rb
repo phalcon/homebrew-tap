357a43774157c8bb4a8d8b050ed915a188b5c8fa
Paul Scarrone <ninjapanzer@gmail.com>
2020-01-08 07:12:27 -0500

    Added reference to catalina bottle

--- Formula/phalcon@4.0.0.rb
+++ Formula/phalcon@4.0.0.rb
@@ -11,6 +11,7 @@ class PhalconAT400 < AbstractPhp74Extension
   bottle do
     cellar :any_skip_relocation
     root_url "https://github.com/phalcon/homebrew-tap/releases/download/v4.0.x"
+    sha256 "607eaf8613d7d02dd0529fd641978e2a814cb525d502ab882aded143bcda97e4" => :catalina
     sha256 "e645dfbf9cb485380f7cd92740dc5dcd30f8b6f4b9051ee3f8ec11f903e9243a" => :mojave
     sha256 "2b827bbdd2b52f136af1cfa1e209457f0264c5d9a0804345982e0a57d50aa940" => :high_sierra
     sha256 "8f04465129e223fa9e261a73ce3a9d7cdecf6786890c854bd38b560852ed6ec6" => :sierra
