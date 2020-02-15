7828515c62742961fd20181ef627524b416aa54c
Paul Scarrone <ninjapanzer@gmail.com>
2020-02-15 17:18:50 -0500

    Update phalcon@4.0.4.rb

--- Formula/phalcon@4.0.4.rb
+++ Formula/phalcon@4.0.4.rb
@@ -11,7 +11,7 @@ class PhalconAT404 < AbstractPhp74Extension
   bottle do
     cellar :any_skip_relocation
     root_url "https://github.com/phalcon/homebrew-tap/releases/download/v4.0.4"
-    #sha256 "7f1c17ad8fd82b61fae2c7a62fe5c8a1f30676136297a017a6cd9f39fd5c9f25" => :catalina
+    sha256 "6c298766d90e32147532d1f7b995a65394e638cc41350514babb96c0ee31eb2d" => :catalina
     # sha256 "49576fba7362f29fb06fa5586bbe92473bd4c7f051eb88ef3d159e68cebb7728" => :mojave
     # sha256 "20b862ff1c24b8fa33462f01c61a10e2f08fa67a1a94be69df38af662d893398" => :high_sierra
     # sha256 "4df08e30b0147234585ec94639fc01c8d80767dcb77f6c4cfe1df3943d4ee144" => :sierra
