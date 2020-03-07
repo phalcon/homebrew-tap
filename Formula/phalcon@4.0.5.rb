a9153d045d3469b4d8fc7a4db4cb8aeb50b8583f
Paul Scarrone <paul.scarrone@gmail.com>
2020-03-07 15:25:46 -0500

    Added bottle reference for php 7.4 on catalina

--- Formula/phalcon@4.0.5.rb
+++ Formula/phalcon@4.0.5.rb
@@ -11,7 +11,7 @@ class PhalconAT405 < AbstractPhp74Extension
   bottle do
     cellar :any_skip_relocation
     root_url "https://github.com/phalcon/homebrew-tap/releases/download/v4.0.5"
-    # sha256 "6c298766d90e32147532d1f7b995a65394e638cc41350514babb96c0ee31eb2d" => :catalina
+    sha256 "d429b4580bdcfbc8b4ac423c15a913d593856b84b00fdc59bc66296c15c8a2ec" => :catalina
     # sha256 "49576fba7362f29fb06fa5586bbe92473bd4c7f051eb88ef3d159e68cebb7728" => :mojave
     # sha256 "20b862ff1c24b8fa33462f01c61a10e2f08fa67a1a94be69df38af662d893398" => :high_sierra
     # sha256 "4df08e30b0147234585ec94639fc01c8d80767dcb77f6c4cfe1df3943d4ee144" => :sierra
