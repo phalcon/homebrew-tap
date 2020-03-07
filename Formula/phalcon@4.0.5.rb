e77ac33e1ea5c0ad61d871c9edb3bc8fff582896
Paul Scarrone <paul.scarrone@gmail.com>
2020-03-07 14:56:47 -0500

    Added formula for phalcon 4.0.5 bumped psr to 1.0.0

---
+++ Formula/phalcon@4.0.5.rb
@@ -0,0 +1,34 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class PhalconAT405 < AbstractPhp74Extension
+  init
+  desc "Full-stack PHP framework"
+  homepage "https://phalconphp.com/"
+  url "https://github.com/phalcon/cphalcon/archive/v4.0.5.tar.gz"
+  sha256 "b6b618911a4dfe60ba63708d92811d4885b8b5220c45981ff1cb7750523074f4"
+  head "https://github.com/phalcon/cphalcon.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://github.com/phalcon/homebrew-tap/releases/download/v4.0.5"
+    # sha256 "6c298766d90e32147532d1f7b995a65394e638cc41350514babb96c0ee31eb2d" => :catalina
+    # sha256 "49576fba7362f29fb06fa5586bbe92473bd4c7f051eb88ef3d159e68cebb7728" => :mojave
+    # sha256 "20b862ff1c24b8fa33462f01c61a10e2f08fa67a1a94be69df38af662d893398" => :high_sierra
+    # sha256 "4df08e30b0147234585ec94639fc01c8d80767dcb77f6c4cfe1df3943d4ee144" => :sierra
+  end
+
+  depends_on "pcre"
+  depends_on "psr"
+
+  def install
+    Dir.chdir "build/php7/64bits"
+
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-phalcon"
+    system "make"
+
+    prefix.install "modules/phalcon.so"
+    write_config_file if build.with? "config-file"
+  end
+end
--- Formula/psr@74.rb
+++ Formula/psr@74.rb
@@ -4,14 +4,14 @@ class PsrAT74 < AbstractPhp74Extension
   init
   desc "PHP extension providing the accepted PSR interfaces "
   homepage "https://phalconphp.com/"
-  url "https://github.com/jbboehr/php-psr/archive/v0.7.0.tar.gz"
-  sha256 "648aac07414f8c6e5c80728cf91fa8174bbd18dd41ae1a90168b510a507cf805"
+  url "https://github.com/jbboehr/php-psr/archive/v1.0.0.tar.gz"
+  sha256 "f85be1d1434368abd16e06b81e394487f81b5e2706220f01c85558ba486ee3e7"
   head "https://github.com/jbboehr/php-psr.git"
 
   bottle do
     cellar :any_skip_relocation
     root_url "https://github.com/phalcon/homebrew-tap/releases/download/v4.0.x"
-    sha256 "06249d93567e517f25dabf0e92e0b9b2c04562d2ad1c28b734973d76e94508cb" => :sierra
+    # sha256 "06249d93567e517f25dabf0e92e0b9b2c04562d2ad1c28b734973d76e94508cb" => :sierra
   end
 
   depends_on "pcre"
