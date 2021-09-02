ca0cbe6ead54768455e152edf97323b9a7e5edc0
AlexNDRmac <alexndr.mac@gmail.com>
2021-09-02 19:59:17 +0300

    Use old psr version

--- Formula/psr@74.rb
+++ Formula/psr@74.rb
@@ -4,7 +4,7 @@ class PsrAT74 < AbstractPhp74Extension
   init
   desc "PHP extension providing the accepted PSR interfaces "
   homepage "https://phalconphp.com/"
-  url "https://github.com/jbboehr/php-psr/archive/v1.0.0.tar.gz"
+  url "https://github.com/jbboehr/php-psr/archive/v0.7.0.tar.gz"
   sha256 "f85be1d1434368abd16e06b81e394487f81b5e2706220f01c85558ba486ee3e7"
   head "https://github.com/jbboehr/php-psr.git"
 
